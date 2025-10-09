4661e862313ff5ab67a4f468a4f19739907f0f01
maxnedu <toptopun3@gmail.com>
2025-10-09 09:08:52 +0200

    ggg

--- my codes/Untitled-1.c
+++ my codes/Untitled-1.c
@@ -1,8 +1,43 @@
+
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
 
 int main() {
+    char znak;
+    int cislo_uz;
+    int pocet = 0;
+    printf("Hadej cislo(1 - 100):");
+    scanf("%d", &cislo_uz);
     
-    printf("Hello World");
-
+    srand(time(NULL));
+    int cislo_ran = rand() % 100 + 1;
+    do {
+    while(cislo_uz != cislo_ran) {
+        
+        if(cislo_uz < cislo_ran) {
+         printf("Moc nizko\n");
+         pocet++;
+         printf("Hadej cislo(1-100):");
+         scanf("%d", &cislo_uz);
+        }
+      
+       else if(cislo_uz > cislo_ran){
+        printf("Moc vysoko\n");
+        pocet++;
+        printf("Hadej cislo(1-100):");
+        scanf("%d", &cislo_uz);
+        
+        }
+     
+   }
+   printf("Uhodl jsi! Pocet pokusu:%d\n", pocet);
+   printf("Chces hrat znovu?(a/n):");
+   scanf(" %c", &znak);
+    } while(znak == 'a');
+   
+   
+   
     return 0;
 }
