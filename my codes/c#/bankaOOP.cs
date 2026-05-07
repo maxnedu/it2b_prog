003262cf2f0a19e34f3679e4a050e35b1cda346b
Maksym Neduzhyi <toptopun3@gmail.com>
2026-05-07 09:07:38 +0200

    Update bankaOOP.cs

--- my codes/c#/bankaOOP.cs
+++ my codes/c#/bankaOOP.cs
@@ -1,15 +1,27 @@
 class Banka
 {
-    public int Bezny {  get; set; }
-    public int Sporici {  get; set; }
+    public int Bezny { get; set; }
+    public int Sporici { get; set; }
+    private string Heslo { get; set; }
 
 
-    public Banka(int bezny, int sporici)
+    public Banka(int bezny, int sporici, string heslo)
     {
         Bezny = bezny;
         Sporici = sporici;
+        Heslo = heslo;
     }
-
+    
+    public bool Autorizace(string heslo)
+    {
+        if(heslo != Heslo)
+        {
+            return false;
+        } else
+        {
+            return true;
+        }
+    }
     public int ZobrazitZustatekB()
     {
         return Bezny;
@@ -20,13 +32,13 @@ class Banka
         return Sporici;
     }
 
-    public int Vlozit(int vloz_penize)
+    public int Vlozit(int vloz_penize = 234)
     {
         Bezny += vloz_penize;
         return Bezny;
     }
 
-    public int Vyber(int vyber_penez)
+    public int Vyber(int vyber_penez = 100)
     {
         Bezny -= vyber_penez;
         return Bezny;
