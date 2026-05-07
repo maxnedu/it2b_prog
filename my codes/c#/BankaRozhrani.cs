7938d24d5685a0e7943a05cea3348cdc0e1d32fa
Maksym Neduzhyi <toptopun3@gmail.com>
2026-05-07 09:07:06 +0200

    Update BankaRozhrani.cs

--- my codes/c#/BankaRozhrani.cs
+++ my codes/c#/BankaRozhrani.cs
@@ -1,46 +1,52 @@
 class Program
 {
-   static void Main()
+    static void Main()
     {
         bool pokracovat = true;
         int vyber;
-        Banka banka = new Banka(10000, 2000);
-        
-        while (pokracovat)
+        Banka banka = new Banka(10000, 2000, "password");
+        if (banka.Autorizace("password") == true)
         {
-            Console.WriteLine("Vyber operace:");
-            Console.WriteLine("1) Zobrazit zustatek na beznem uctu");
-            Console.WriteLine("2) Zobrazit zustatek na sporicim uctu");
-            Console.WriteLine("3) Vlozit penize");
-            Console.WriteLine("4) Vyber penez");
-            Console.WriteLine("5) Ukoncit program");
-            vyber = int.Parse(Console.ReadLine());
-
-            switch (vyber)
+            Console.WriteLine("Autorizace probehla uspesne!");
+            while (pokracovat)
             {
-                case 1:
-                    Console.WriteLine(banka.ZobrazitZustatekB());
-                    break;
-                case 2: 
-                    Console.WriteLine(banka.ZobrazitZustatekS());
-                    break;
-                case 3:
-                    Console.WriteLine("Kolik chcete vlozit?");
-                    int vloz = int.Parse(Console.ReadLine());
-                    banka.Vlozit(vloz);
-                    break;
-                case 4:
-                    Console.WriteLine("Kolik chcete vybrat?");
-                    int vybrat = int.Parse(Console.ReadLine());
-                    banka.Vyber(vybrat);
-                    break;
-                case 5:
-                    pokracovat = false;
-                    break;
-                default:
-                    Console.WriteLine("Spatny vstup");
-                    break;
+                Console.WriteLine("Vyber operace:");
+                Console.WriteLine("1) Zobrazit zustatek na beznem uctu");
+                Console.WriteLine("2) Zobrazit zustatek na sporicim uctu");
+                Console.WriteLine("3) Vlozit penize");
+                Console.WriteLine("4) Vyber penez");
+                Console.WriteLine("5) Ukoncit program");
+                vyber = int.Parse(Console.ReadLine());
+
+                switch (vyber)
+                {
+                    case 1:
+                        Console.WriteLine(banka.ZobrazitZustatekB());
+                        break;
+                    case 2:
+                        Console.WriteLine(banka.ZobrazitZustatekS());
+                        break;
+                    case 3:
+                        Console.WriteLine("Kolik chcete vlozit?");
+                        int vlozit =int.Parse(Console.ReadLine());
+                        banka.Vlozit(vlozit);
+                        break;
+                    case 4:
+                        Console.WriteLine("Kolik chcete vybrat?");
+                        int vybrat = int.Parse(Console.ReadLine());
+                        banka.Vyber(vybrat);
+                        break;
+                    case 5:
+                        pokracovat = false;
+                        break;
+                    default:
+                        Console.WriteLine("Spatny vstup");
+                        break;
+                }
             }
+        }else
+        {
+            Console.WriteLine("Spatne heslo");
         }
     }
 }
